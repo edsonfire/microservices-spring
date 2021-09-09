86ee2d2537ee19f1fc2360c6ff251f28aef90ed4
unknown <edsonfire@gmail.com>
2021-09-09 00:35:46 -0300

    projeto

---
+++ src/main/java/br/com/prime/live/projetomicroservicos/ProjetoMicroservicosApplication.java
@@ -0,0 +1,13 @@
+package br.com.prime.live.projetomicroservicos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoMicroservicosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoMicroservicosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/prime/live/projetomicroservicos/ProjetoMicroservicosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.prime.live.projetomicroservicos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoMicroservicosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
